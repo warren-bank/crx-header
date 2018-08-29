0aea26fdb7ee2aceec394c8f376e0919f5bb26d5
Warren R Bank <warren.r.bank@gmail.com>
2018-08-28 19:12:15 -0700

    v0.2.0 - print encoded binary data in both hex and base64

--- bin/crx-header.pl
+++ bin/crx-header.pl
@@ -1,6 +1,16 @@
 #!/usr/bin/env perl
 # Analyze header of CRX packages (Chrome extensions).
 
+# --------------------------------------------------------------------
+# credits to:
+# --------------------------------------------------------------------
+# https://gist.github.com/mwgamera/9774270
+# https://gist.github.com/mwgamera/9774270/raw/4f8541e1b13137646c2cb9e1fd8636ccc03320a4/crxhdr.pl
+# --------------------------------------------------------------------
+# ftp://ftp.squid-cache.org/pub/squid/contrib/no_check/no_check.pl
+#   * sub encode_base64 ($;$) {}
+# --------------------------------------------------------------------
+
 $CRX_MAGIC = 0x34327243;
 
 sub print_hex {
@@ -9,6 +19,23 @@ sub print_hex {
   foreach (@bytes) {
     printf "%02lx", ord $_;
   }
+}
+
+sub print_base64 {
+  my $data = shift(@_);
+  my $res = "";
+  pos($data) = 0;
+  while ($data =~ /(.{1,45})/gs) {
+    $res .= substr(pack('u', $1), 1);
+    chop($res);
+  }
+  $res =~ tr|` -_|AA-Za-z0-9+/|;
+
+  # fix padding at the end
+  my $padding = (3 - length($data) % 3) % 3;
+  $res =~ s/.{$padding}$/'=' x $padding/e if $padding;
+
+  printf "$res";
 }
 
 do {
@@ -39,14 +66,24 @@ do {
   read $fh, my $pk, $pklen or die $!;
   read $fh, my $sig, $siglen or die $!;
 
-  printf "public key:\n";
-  printf "===========\n";
+  printf "public key (hex):\n";
+  printf "=================\n";
   print_hex($pk);
   printf "\n\n";
 
-  printf "signature:\n";
-  printf "==========\n";
+  printf "public key (base64):\n";
+  printf "====================\n";
+  print_base64($pk);
+  printf "\n\n";
+
+  printf "signature (hex):\n";
+  printf "================\n";
   print_hex($sig);
+  printf "\n\n";
+
+  printf "signature (base64):\n";
+  printf "===================\n";
+  print_base64($sig);
   printf "\n\n";
 
   close $fh;
