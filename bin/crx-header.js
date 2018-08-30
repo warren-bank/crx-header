7f2a79037a517f8df2a1847179c1fd1a44378313
Warren R Bank <warren.r.bank@gmail.com>
2018-08-29 20:42:56 -0700

    v0.2.0 - verify the CRX signature was generated with the public key

--- bin/crx-header.js
+++ bin/crx-header.js
@@ -28,49 +28,57 @@ args.forEach((filepath) => {
   try {
     fs.accessSync(filepath, fs.constants.F_OK | fs.constants.R_OK)
   } catch (error) {
-    console.log('ERROR:', 'filepath to input CRX must exist and be readable', "\n", filepath, "\n")
+    console.log('ERROR:', filepath)
+    console.log('ERROR:', 'filepath to input CRX must exist and be readable', "\n")
     return // process next input filepath
   }
 
-  const crxBuffer   = fs.readFileSync(filepath, {encoding: null})
-  const crxReader   = new CrxReader(crxBuffer)
-  const hexContents = crxReader.getContents('hex')
-  const b64Contents = crxReader.getContents('base64')
+  try {
+    const crxBuffer   = fs.readFileSync(filepath, {encoding: null})
+    const crxReader   = new CrxReader(crxBuffer)
+    const hexContents = crxReader.getContents('hex')
+    const b64Contents = crxReader.getContents('base64')
 
-  if (!counter) print_divider()
+    if (!counter) print_divider()
 
-  console.log('filename:')
-  console.log('=========')
-  console.log(filepath)
-  console.log()
+    console.log('filename:')
+    console.log('=========')
+    console.log(filepath)
+    console.log()
 
-  console.log('version = ',              hexContents.version)
-  console.log('length of public key = ', hexContents.publicKeyLength)
-  console.log('length of signature  = ', hexContents.signatureLength)
-  console.log()
+    console.log('version =',              hexContents.version)
+    console.log('length of public key =', hexContents.publicKeyLength)
+    console.log('length of signature  =', hexContents.signatureLength)
+    console.log()
 
-  console.log('public key (hex):')
-  console.log('=================')
-  console.log(hexContents.publicKey)
-  console.log()
+    console.log('public key (hex):')
+    console.log('=================')
+    console.log(hexContents.publicKey)
+    console.log()
 
-  console.log('public key (base64):')
-  console.log('====================')
-  console.log(b64Contents.publicKey)
-  console.log()
+    console.log('public key (base64):')
+    console.log('====================')
+    console.log(b64Contents.publicKey)
+    console.log()
 
-  console.log('signature (hex):')
-  console.log('=================')
-  console.log(hexContents.signature)
-  console.log()
+    console.log('signature (hex):')
+    console.log('=================')
+    console.log(hexContents.signature)
+    console.log()
 
-  console.log('signature (base64):')
-  console.log('====================')
-  console.log(b64Contents.signature)
-  console.log()
+    console.log('signature (base64):')
+    console.log('====================')
+    console.log(b64Contents.signature)
+    console.log()
 
-  print_divider()
-  counter++
+    print_divider()
+    counter++
+  }
+  catch (error) {
+    console.log('ERROR:', filepath)
+    console.log('ERROR:', error.message, "\n")
+    return // process next input filepath
+  }
 })
 
 if (counter === 0) {
